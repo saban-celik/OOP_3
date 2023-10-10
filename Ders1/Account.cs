6d91878cce42f557d771543a96ebba8c1233cb67
Saban <Saban@DESKTOP-8IO52BT>
2023-10-10 14:13:11 +0300

    Add project files.

---
+++ Ders1/Account.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Account
+{
+    public string AccountNumber { get; }
+    public string AccountHolder { get; }
+    public decimal Balance { get; private set; }
+    private List<string> TransactionHistory { get; }
+    public string Password { get; }
+
+    public Account(string accountNumber, string accountHolder, decimal balance, string password)
+    {
+        AccountNumber = accountNumber;
+        AccountHolder = accountHolder;
+        Balance = balance;
+        Password = password;
+        TransactionHistory = new List<string>();
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+            TransactionHistory.Add($"{amount} TL yatırıldı. Yeni bakiye: {Balance} TL");
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz miktar. Lütfen pozitif bir miktar girin.");
+        }
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount >= 50 && amount <= 10000)
+        {
+            if (amount > 0 && amount <= Balance)
+            {
+                Balance -= amount;
+                TransactionHistory.Add($"{amount} TL çekildi. Yeni bakiye: {Balance} TL");
+            }
+            else
+            {
+                Console.WriteLine("Yetersiz bakiye veya geçersiz miktar. İşlem gerçekleştirilemedi.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz miktar. Çekilecek para 50 ile 10,000 TL arasında olmalıdır.");
+        }
+    }
+
+    public void CheckAccount()
+    {
+        Console.WriteLine($"Hesap Numarası: {AccountNumber}");
+        Console.WriteLine($"Hesap Sahibi: {AccountHolder}");
+        Console.WriteLine($"Bakiye: {Balance} TL");
+    }
+
+    public void AccountActivities()
+    {
+        Console.WriteLine("Hesap hareketleri listeleniyor...");
+        foreach (var transaction in TransactionHistory)
+        {
+            Console.WriteLine(transaction);
+        }
+    }
+
+    public bool VerifyPassword(string passwordToVerify)
+    {
+        return Password == passwordToVerify;
+    }
+}
---
+++ Ders1/Program.cs
@@ -0,0 +1,95 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        List<Account> accounts = new List<Account>
+        {
+            new Account("12345", "John Doe", 1000, "password1"),
+            new Account("67890", "Jane Smith", 500, "password2"),
+            // Diğer hesaplar burada...
+        };
+
+        Console.WriteLine("Hesap Numarası: ");
+        string accountNumber = Console.ReadLine();
+
+        Console.WriteLine("Şifre: ");
+        string password = Console.ReadLine();
+
+        Account loggedInAccount = null;
+
+        foreach (var account in accounts)
+        {
+            if (account.AccountNumber == accountNumber && account.VerifyPassword(password))
+            {
+                loggedInAccount = account;
+                break;
+            }
+        }
+
+        if (loggedInAccount == null)
+        {
+            Console.WriteLine("Geçersiz hesap numarası veya şifre.");
+            return;
+        }
+
+        Console.WriteLine($"Hoş geldiniz, {loggedInAccount.AccountHolder}!");
+
+        while (true)
+        {
+            Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçin:");
+            Console.WriteLine("1. Para Yatırma");
+            Console.WriteLine("2. Para Çekme");
+            Console.WriteLine("3. Hesap Bilgilerini Gösterme");
+            Console.WriteLine("4. Hesap Hareketlerini Gösterme");
+            Console.WriteLine("5. Çıkış");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Yatırmak istediğiniz miktarı girin: ");
+                    decimal depositAmount;
+                    if (decimal.TryParse(Console.ReadLine(), out depositAmount))
+                    {
+                        loggedInAccount.Deposit(depositAmount);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Geçersiz miktar. Lütfen pozitif bir miktar girin.");
+                    }
+                    break;
+
+                case "2":
+                    Console.Write("Çekmek istediğiniz miktarı girin: ");
+                    decimal withdrawAmount;
+                    if (decimal.TryParse(Console.ReadLine(), out withdrawAmount))
+                    {
+                        loggedInAccount.Withdraw(withdrawAmount);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Geçersiz miktar. Lütfen pozitif bir miktar girin.");
+                    }
+                    break;
+
+                case "3":
+                    loggedInAccount.CheckAccount();
+                    break;
+
+                case "4":
+                    loggedInAccount.AccountActivities();
+                    break;
+
+                case "5":
+                    Console.WriteLine("Çıkış yapılıyor...");
+                    return;
+
+                default:
+                    Console.WriteLine("Geçersiz seçenek. Lütfen tekrar deneyin.");
+                    break;
+            }
+        }
+    }
+}
